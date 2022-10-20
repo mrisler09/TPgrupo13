6132a641b67f420340eeedc99d88567681a7981d
SilSch <113948122+SilSch@users.noreply.github.com>
2022-10-19 22:33:32 -0300

    Add files via upload
    Hay que ver el tema de los estilos de letra y que sea responive... agregar Api y mejorarla muchoooo

---
+++ index.js
@@ -0,0 +1,31 @@
+var cad=`
+<div class="flex-conteiner">                      
+    <div class="flex-item">
+        <img src="Img/Imagen.jpg" id="reducir" alt="">     
+    </div>
+    <div class="flex-item">
+        <h3>DATOS DE CONTACTO</h3>
+        <P>Corrientes 1234</P>
+        <p>CABA</p>
+        <p>Bs.As</p>
+        <p>Tel:11111111</p>
+               
+    </div>
+    <div class="flex-item">
+        <h3>SIGUENOS:</h3>
+        <br>
+        <div class="sociales">
+            <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+            <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+            <a href="#" class="youtube"><i class="fa fa-youtube-play"></i></a>
+            <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                        
+        </div>
+    </div>
+</div>
+        
+<div class="derechos">
+    <small>& copy; 2022<b>Comision 22533 - Grupo13 </b>Todos los Derechos Reservados</small> 
+</div>
+`
+document.getElementById("Idpiepagina").innerHTML=cad
